a210f11161995d0f99776efd61457fd999ab775b
Matthew Nickson <mnickson@sidingsmedia.com>
2022-09-28 10:24:54 +0100

    Added docstrings

--- cardgame/Collection.py
+++ cardgame/Collection.py
@@ -3,20 +3,38 @@
 
 import random
 
-from Card import Card
+import Card
 
 
 class Collection:
     def __init__(self) -> None:
+        """Collection is a base class for holding sets of cards"""
+        
         self._cards = []
 
     def addCard(self, card: Card.Card) -> int:
+        """
+        Add a card to the collection
+
+        :param card: The card to add
+        :type card: Card.Card
+        :returns: Index card was added at
+        :rtype: int
+        """
+        
         self._cards.append(card)
         return len(self._cards) - 1
 
     def shuffle(self) -> None:
+        """
+        Pseudo random shuffle the cards
+        Uses random.shuffle for pseudo random shuffling
+        """
+        
         random.shuffle(self._cards)
 
     def printDeck(self) -> None:
+        """Output deck to console"""
+        
         for i in self._cards:
-            print(f"{}"
+            print(f"{i.getParsedValue()} {i.suit}")

83f5271a029483fdcbf163718fd72587e4e4c630
Matthew Nickson <mnickson@sidingsmedia.com>
2022-09-30 15:31:13 +0100

    Added start of pile

--- cardgame/Collection.py
+++ cardgame/Collection.py
@@ -9,9 +9,20 @@ import Card
 
 class Collection:
     def __init__(self) -> None:
-        """Collection is a base class for holding sets of cards"""
+        """
+        Collection is a base class for holding sets of cards
+
+        The cards are represented as an array with the end of said
+        array representing the top of the deck.
+        """
         
         self._cards = []
+
+    @property
+    def cards(self) -> list[Card.Card]:
+        """List of all cards in collection. 0 is bottom card"""
+        
+        return self._cards
 
     def addCard(self, card: Card.Card) -> int:
         """
@@ -25,6 +36,28 @@ class Collection:
 
         self._cards.append(card)
         return len(self._cards) - 1
+
+    def addCardByIndex(self, card: Card.Card, index: int) -> None:
+        """
+        Add a card to the collection at the specified index
+
+        :param card: The card to add
+        :type card: Card.Card
+        :param index: Index to add card at
+        :type index: int
+        """
+
+        self._cards.insert(index, card)
+
+    def addCardBottom(self, card: Card.Card) -> None:
+        """
+        Add a card to the bottom of the collection
+
+        :param card: The card to add
+        :type card: Card.Card
+        """
+
+        self.addCardByIndex(card, 0)
 
     def removeCard(self, card: Union[Card.Card,int]) -> Card.Card:
         """
@@ -55,3 +88,39 @@ class Collection:
         
         for i in self._cards:
             print(f"{i.getParsedValue()} {i.suit}")
+
+
+class Deck(Collection):
+    def __init__(self, ace_high = False) -> None:
+        """
+        Create an instance of Deck
+
+        :param ace_high: Should a high or low ace be used?, defaults to False
+        :type ace_high: bool, optional
+        """
+        
+        super().__init__()
+        self._generateDeck(ace_high)
+        self.shuffle()
+
+    def _generateDeck(self, ace_high: bool ) -> None:
+        """
+        Generate a deck
+
+        :param ace_high: Should Ace be 1 or 14. Set to True if 14
+        :type ace_high: bool
+        """
+        
+        suits = ["hearts", "dimonds", "clubs", "spades"]
+        if ace_high:
+           card_range = (2,15)
+        else:
+            card_range = (1,14)
+            
+        for i in suits:
+            for j in range(card_range[0], card_range[1])
+
+
+class Pile(Collection):
+    
+    
